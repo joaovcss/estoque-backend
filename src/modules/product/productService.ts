10f8397ac9117144b2913b225aaa5dafa1e23da8
João Victor <joaovictorcarvalhosilva29@gmail.com>
2026-04-14 20:12:34 -0300

    feat: add findAll, findById and create functions to productService

---
+++ src/modules/product/productService.ts
@@ -0,0 +1,23 @@
+import { CreateProductDTO } from "./productDTO";
+import { Product } from "./productModel";
+import { ProductRepository } from "./productRepository";
+
+export class ProductService {
+  constructor(private productRepository: ProductRepository) {}
+
+  async findAll(): Promise<Product[]> {
+    return this.productRepository.findAll()
+   }
+
+  async findById(id: number): Promise<Product> {
+    return this.productRepository.findById(id)
+  }
+
+  async create(data: CreateProductDTO): Promise<Product> {
+    if(!data.nome) {
+      throw new Error("Nome é obrigatório!")
+    }
+
+    return this.productRepository.create(data)
+  }
+}
