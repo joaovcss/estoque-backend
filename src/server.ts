7a0f711f03c47f53b984bd8157b77b1b67bc3535
João Victor <joaovictorcarvalhosilva29@gmail.com>
2026-04-14 20:15:15 -0300

    feat: add router to express server

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
 import express from "express";
+import { router } from "./routes/routes";
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 app.use(express.json())
+app.use("/estoque", router)
 
 app.get("/", (req, res) => {
   res.json({ message: "Servidor rodando!"})
