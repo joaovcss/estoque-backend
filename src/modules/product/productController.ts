02a23b06c6019ecfe373c541725b4c060597760f
João Victor <joaovictorcarvalhosilva29@gmail.com>
2026-04-19 15:54:51 -0300

    feat: implemented product registration

--- src/modules/product/productController.ts
+++ src/modules/product/productController.ts
@@ -8,4 +8,9 @@ export class ProductController {
     const products = await this.productService.findAll()
     res.status(200).json(products)
   }
+
+  create: Handler = async (req, res) => {
+    const product = await this.productService.create(req.body)
+    res.status(201).json(product)
+  }
 } 
--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -4,5 +4,6 @@ import { productController } from "../container";
 const router = Router()
 
 router.get("/produtos", productController.findAll)
+router.post("/produtos", productController.create)
 
 export { router }
